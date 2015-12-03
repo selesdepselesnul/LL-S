650839d7ac490208cc4a8653764ebf7f82ec4359
selesdepselesnul <selesdepselesnul@gmail.com>
2015-12-03 08:06:14 +0700

    init project

---
+++ Selesdepselesnul/LinkedList.hs
@@ -0,0 +1,29 @@
+-- Nama  : Moch Deden R S
+-- Kelas : A2
+-- Npm   : 41155050140062
+module Selesdepselesnul.LinkedList
+    (
+    LinkedList(..)
+    , display
+    , deleteHead
+    , search
+    ) where
+infixr 5 :-:
+data LinkedList a =
+    EmptyList
+    | a :-: LinkedList a
+    deriving(Show)
+
+display :: LinkedList String -> IO ()
+display EmptyList = putStrLn "Kosong"
+display (x :-: EmptyList) = putStrLn  x
+display (x :-: xs) = putStrLn x >> display xs
+
+deleteHead :: LinkedList String -> LinkedList String
+deleteHead (x :-: xs) = xs
+deleteHead _ = EmptyList
+
+search :: String -> LinkedList String -> Bool
+search _ EmptyList = False
+search a (x :-: EmptyList) = a == x
+search a (x :-: xs) = x == a || search a xs
---
+++ Selesdepselesnul/Main.hs
@@ -0,0 +1,46 @@
+-- Nama  : Moch Deden R S
+-- Kelas : A2
+-- Npm   : 41155050140062
+module Selesdepselesnul.Main where
+import Selesdepselesnul.LinkedList
+
+askMenu :: LinkedList String -> IO ()
+askMenu linkedList = do
+    putStrLn "Masukan pilihan : "
+    putStrLn "1. Insert"
+    putStrLn "2. Search"
+    putStrLn "3. Delete"
+    putStrLn "4. Display"
+    putStrLn "5. Exit"
+    putStrLn "-> "
+    choice <- getLine
+    handleChoice choice
+    where handleChoice x
+            | x == "1" = do
+                putStrLn $ "Masukan nilainya -> "
+                newValue <- getLine
+                putStrLn $ "Anda memasukan nilai -> " ++ newValue
+                askMenu $ newValue :-: linkedList
+            | x == "2" = do
+                putStrLn "Masukan nilainya yang hendak dicari : "
+                searchedValue <- getLine
+                if search searchedValue linkedList then
+                    putStrLn "Ditemukan"
+                else
+                    putStrLn "Tidak ditemukan"
+                askMenu linkedList
+            | x == "3" = do
+                putStrLn "Bagian head dihapus, isi linked-list sekarang : "
+                let newLinkedList = deleteHead linkedList
+                display newLinkedList
+                askMenu newLinkedList
+            | x == "4" = do
+                putStrLn "Isi linked-list :"
+                display $ linkedList
+                askMenu linkedList
+            | x == "5" = putStrLn "Program Keluar!"
+            | otherwise = putStrLn "Pilihan tidak valid!"
+
+main :: IO ()
+main = do
+    askMenu EmptyList
