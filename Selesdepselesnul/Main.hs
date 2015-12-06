86fc0b5e420370c3e31b6374de0359e0b264ad1c
Moch Deden <selesdepselesnul@gmail.com>
2015-12-06 09:00:49 +0700

    refactor

--- Selesdepselesnul/Main.hs
+++ Selesdepselesnul/Main.hs
@@ -1,53 +1,65 @@
 -- author  : Moch Deden(https://github.com/selesdepselesnul)
+-- Nama  : Moch Deden R S
+-- Npm   : 41155050140062
+-- Kelas : A2
 module Main where
 import Selesdepselesnul.LinkedList
 import System.Process
 import System.Info as Info
 
-clear = 
-    if Info.os == "linux" || Info.os == "darwin" then
-        system "clear"
-    else
-        system "cls"
+main :: IO ()
+main = do
+    askMenu EmptyList
 
 askMenu :: LinkedList String -> IO ()
-askMenu linkedList = do
-    putStrLn "Masukan pilihan : "
-    putStrLn "1. Insert"
-    putStrLn "2. Search"
-    putStrLn "3. Delete"
-    putStrLn "4. Display"
-    putStrLn "5. Exit"
-    putStrLn "-> "
-    choice <- getLine
-    clear
-    handleChoice choice
-    where handleChoice x
-            | x == "1" = do
-                putStrLn $ "Masukan nilainya -> "
+askMenu linkedList =
+    displayMenu >> getLine >>= \x -> clear >> handleChoice x
+
+    where
+        displayMenu = do
+            putStrLn "Masukan pilihan : "
+            putStrLn "1. Insert"
+            putStrLn "2. Search"
+            putStrLn "3. Delete"
+            putStrLn "4. Display"
+            putStrLn "5. Exit"
+            putStrLn "->"
+
+        clear = do
+            if Info.os == linux || Info.os == osX then
+                system "clear"
+            else
+                system "cls"
+            return ()
+            where
+                linux = "linux"
+                osX = "darwin"
+
+        handleChoice choice
+            | choice == "1" = do
+                putStrLn "Masukan nilainya -> "
                 newValue <- getLine
                 putStrLn $ "Anda memasukan nilai -> " ++ newValue
                 askMenu $ newValue :-: linkedList
-            | x == "2" = do
+            | choice == "2" = do
                 putStrLn "Masukan nilainya yang hendak dicari : "
                 searchedValue <- getLine
-                if search searchedValue linkedList then
+                if searchedValue `isIn` linkedList then
                     putStrLn "Ditemukan"
                 else
                     putStrLn "Tidak ditemukan"
                 askMenu linkedList
-            | x == "3" = do
+            | choice == "3" = do
                 putStrLn "Bagian head dihapus, isi linked-list sekarang : "
                 let newLinkedList = deleteHead linkedList
                 display newLinkedList
                 askMenu newLinkedList
-            | x == "4" = do
+            | choice == "4" = do
                 putStrLn "Isi linked-list :"
-                display $ linkedList
+                display linkedList
                 askMenu linkedList
-            | x == "5" = putStrLn "Program Keluar!"
-            | otherwise = putStrLn "Pilihan tidak valid!" >> askMenu linkedList
+            | choice == "5" = putStrLn "Program Keluar!"
+            | otherwise = do
+                putStrLn "Pilihan tidak valid!"
+                askMenu linkedList
 
-main :: IO ()
-main = do
-    askMenu EmptyList
