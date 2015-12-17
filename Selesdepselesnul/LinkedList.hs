faf77659f490ca3b2d067e852a5324b71b9b5f44
selesdepselesnul <selesdepselesnul@gmail.com>
2015-12-17 07:33:23 +0700

    refactor

--- Selesdepselesnul/LinkedList.hs
+++ Selesdepselesnul/LinkedList.hs
@@ -28,13 +28,13 @@ a `isIn` (x :-: xs) = x == a ||  a `isIn` xs
 
 deleteAfter :: Eq a => a -> LinkedList a -> LinkedList a
 deleteAfter _ EmptyList = EmptyList
-deleteAfter x rest@(y :-: EmptyList) = rest
-deleteAfter x (y :-: z :-: zs) = if x == y then y :-: zs  else y :-: (deleteAfter x (z :-: zs))
+deleteAfter pivot list@(y :-: EmptyList) = list
+deleteAfter pivot (x :-: y :-: ys) = if pivot == x then x :-: ys  else x :-: (deleteAfter pivot $ y :-: ys)
 
 insertAfter :: Eq a => a -> a -> LinkedList a -> LinkedList a
 insertAfter _ _ EmptyList = EmptyList 
-insertAfter pivot x rest@(y :-: EmptyList)
-    | pivot /= y = rest 
+insertAfter pivot x list@(y :-: EmptyList)
+    | pivot /= y = list 
 insertAfter pivot x (y :-: ys) 
     | pivot == y = y :-: x :-: ys
     | otherwise = y :-: (insertAfter pivot x ys)
